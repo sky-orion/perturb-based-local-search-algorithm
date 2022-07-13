a94809e68e03ada4c5c260b57c48caa5214f5249
hhaoran <55023820+sky-orion@users.noreply.github.com>
2022-07-13 17:35:18 +0800

    Update PLS1.py

--- PLS/PLS1.py
+++ PLS/PLS1.py
@@ -1,158 +1,76 @@
-import numpy as np
-
-from utils import load_instance
-
-
-class subtour():
-    def __init__(self, depot=0):
-        self.depot = depot
-        self.route = ["d{}".format(depot), "d{}".format(depot)]
-        self.cost = 0
-        self.nowtime = 0  # 现在行车时间
-        self.load = 0
-
-    def reset(self):
-        self.cost = 0
-        self.nowtime = 0  # 现在行车时间
-        self.load = 0
-
-    def insertcusotmer(self, customer, instance):
-        id = customer["id"]
-        name = "c{}".format(id)
-        distance_matrix = instance["distance_matrix"]
-        customerindex = id + instance["number_of_depots"] - 1
-        lastnode = self.route[-2]
-        demand = customer["demand"]
-        if "d" in lastnode:
-            lastnodeindex = int(lastnode.replace("d", ""))
-        else:
-            lastnodeindex = int(lastnode.replace("c", "")) + instance["number_of_depots"] - 1
-        self.nowtime = self.nowtime + distance_matrix[lastnodeindex][customerindex]
-        self.cost = self.cost + self.nowtime
-        self.load = self.load + demand
-        self.route.insert(-1, name)
+from local_search_operator import *
+from utils import improve, calculate_cost
 
 
-def initialization(instance):
-    NV = instance["number_of_vehicles"]
-    ND = instance["number_of_depots"]
-    NC = instance["number_of_customers"]
-    g = NV * ND
-    r = 11  # 11
-    t = min(g, r)  # 允许的车辆数目
-    selectlist = selectroute(instance, t)
-
-    # 每个节点各有number_of_vehicles个车
-    route = []
-    for i in range(ND):
-        for j in range(NV):
-            subroute = subtour(depot=i)
-            route.append(subroute)
-    AT = 0
-    RC = []
-    for i in range(1, NC + 1):
-        name = "customer_{}".format(i)
-        RC.append(instance[name])
-    while len(RC) != 0:
-        RV = []
-        for customer in RC:
-            RV.append(ComputeRegretValue(customer, selectlist, instance, route))
-        rv_array = np.array(RV)
-        rvindex = rv_array.argmax()
-        customeru = RC[rvindex]
-        icindex = Computeinsertioncostall(customeru, route, instance, selectlist)[0][0]
-        e = route[icindex]
-        if len(e.route) != 2:
-            AT = AT + 1
-        RC.pop(rvindex)
-        e.insertcusotmer(customeru, instance)
-        # print(e.cost,insertioncost)
-    newroute = []
-    for i in range(len(route)):
-        subroute = route[i]
-        if len(subroute.route) != 2:
-            newroute.append(subroute)
-    return newroute
-
-
-# 整体路径
-def Computeinsertioncostall(customer, route, instance, selectlist):
-    # 计算插入cost
-    insertioncost = {}
-    for index in selectlist:
-        subroute = route[index]
-        insertioncost[index] = Computeinsertioncost(customer, subroute, instance)
-    sortedinsertioncost = sorted(insertioncost.items(), key=lambda x: x[1])
-    # insertioncost = []
-    # for i in range(len(route)):
-    #     if i >= t:
-    #         break
-    #     subroute = route[i]
-    #     insertioncost.append(Computeinsertioncost(customer, subroute, instance))
-    return sortedinsertioncost
-
-
-# 单条路径
-def Computeinsertioncost(customer, subroute, instance):
-    nowtime = subroute.nowtime
-    route = subroute.route
-    lastnode = route[-2]
-    id = customer["id"]
-    distance_matrix = instance["distance_matrix"]
-    customerindex = id + instance["number_of_depots"] - 1
-    if "d" in lastnode:
-        lastnodeindex = int(lastnode.replace("d", ""))
-    else:
-        lastnodeindex = int(lastnode.replace("c", "")) + instance["number_of_depots"] - 1
-    timeafterlastnode = nowtime + distance_matrix[lastnodeindex][customerindex]
-    # 容量约束
-    depotid = int(route[0].replace("d", ""))
-    if customer["demand"] + subroute.load > instance["depots"][depotid]["maximum_load_of_a_vehicle"]:
-        timeafterlastnode = 99999999
-    return timeafterlastnode
-
-
-def selectroute(instance, t):
+def PLS(instance):
+    s = initialization(instance=instance)
+    lambdaI = 10
+    lambdaO = 10
     sign = False
-    at = 0
-    # 0 2 4 8
-    selectlist = []
-    for i in range(instance["number_of_vehicles"]):
-        if sign:
-            break
-        for j in range(instance["number_of_depots"]):
-            # print(i + instance["number_of_depots"] * j)
-            selectlist.append(i + instance["number_of_vehicles"] * j)
-            at = at + 1
-            if at >= t:
-                sign = True
-                break
-    return selectlist
+    gbest = s
+    wi = 0
+    wo = 0
+    operatorlist = [Relocationmove_operator(instance=instance),
+                    twoExchangemove_operator(instance=instance),
+                    twoOptmove_operator(instance=instance),
+                    ArcNodeExchangemove_operator(instance=instance),
+                    OrOptmove_operator(instance=instance),
+                    # CrossExchangemove_operator(instance=instance, arclenset=2),
+                    # CrossExchangemove_operator(instance=instance, arclenset=3)
+                    ArbitryCrossExchangemove_operator(instance=instance,fastmode=True)
+                    ]
+    random_perturbation_operator = random_perturbation(instance=instance)
+    removeinsert_perturbation_operator = removeinsert_perturbation(instance=instance)
+    globalbest = gbest
+    maxiter = 1000
+    iter = 0
+    while sign is False and iter < maxiter:
+        gbest, update = localsearch(s, operatorlist, globalbest)
+        print("iteration {}| gbest cost {:.2f} ({:.2f})| s cost {:.2f} |global best cost {:.2f}".format(iter,
+                                                                                                        calculate_cost(
+                                                                                                            gbest),
+                                                                                                        calculate_cost(
+                                                                                                            globalbest),
+                                                                                                        calculate_cost(
+                                                                                                            s),
+                                                                                                        calculate_cost(
+                                                                                                            globalbest)))
+        iter = iter + 1
+        if update:
+            wi = 0
+            wo = 0
+            globalbest = gbest
+            s = globalbest
+        if wi < lambdaI:
+            s = random_perturbation_operator.operate(s)
+            print("random_perturbation,wi {},wo {},s {:.2f}".format(wi, wo, calculate_cost(s)))
+            wi = wi + 1
+        elif wo < lambdaO:
+            s = removeinsert_perturbation_operator.operate(s)
+            print("removeinsert_perturbation,wi {},wo {},s {:.2f}".format(wi, wo, calculate_cost(s)))
+            wi = 0
+            wo = wo + 1
+        else:
+            sign = True
+    return globalbest
 
 
-def ComputeRegretValue(customer, selectlist, instance, route):
-    insertioncost = {}
-    # 0 4 8 12
-    # 1 5 9 13
-    for index in selectlist:
-        subroute = route[index]
-        insertioncost[index] = Computeinsertioncost(customer, subroute, instance)
-    # for i in range(g):
-    #     if i >= t:
-    #         break
-    #     subroute = route[i]
-    #     insertioncost.append(Computeinsertioncost(customer, subroute, instance))
-    sortedinsertioncost = sorted(insertioncost.items(), key=lambda x: x[1])
-    RV = sortedinsertioncost[2][1] - sortedinsertioncost[0][1]
-    return RV
+def localsearch(s, operatorlist, globalbest):
+    # print("local search #############")
+    for operator in operatorlist:
+        news = operator.operate(s, random_choice=False)
+        # print(calculate_cost(news))
+        if improve(globalbest, news):  # or improve(lastgbest, news)
+            return news, True
+    return s, False
+
+
+def main():
+    instancename = "p02"
+    instance = load_instance(name=instancename)
+    bestsolution = PLS(instance=instance)
+    print(calculate_cost(bestsolution, True))
 
 
 if __name__ == '__main__':
-    instance = load_instance(name="p02")
-    route = initialization(instance)
-    cost = 0
-    for subroute in route:
-        print(subroute.route, subroute.cost, subroute.load)
-        cost = cost + subroute.cost
-    print("cost", cost)
-    print(selectroute(instance, 2))
+    main()
